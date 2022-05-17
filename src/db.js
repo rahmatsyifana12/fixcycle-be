1586d0d505f7378dee795729e826e638865349b3
rahmatsyifana12 <adean435@gmail.com>
2022-05-17 14:29:34 +0700

    Add db set up

---
+++ src/db.js
@@ -0,0 +1,13 @@
+const Pool = require('pg');
+const config = require('./configs/config');
+
+
+const pool = new Pool({
+    user: config.db.username,
+    password: config.db.password,
+    host: config.db.host,
+    port: config.port,
+    database: config.db.database
+});
+
+module.exports = pool;
