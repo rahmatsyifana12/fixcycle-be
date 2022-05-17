28ea833b458a0951d901ef42dba82bceb88b6dbb
rahmatsyifana12 <adean435@gmail.com>
2022-05-17 14:29:18 +0700

    Add port into config

--- src/configs/config.js
+++ src/configs/config.js
@@ -4,6 +4,14 @@ dotenv.config();
 const { env } = process;
 
 const config = {
+    db: {
+        host: env.DB_HOST,
+        database: env.DB_DATABASE,
+        username: env.DB_USERNAME,
+        password: env.DB_PASSWORD
+    },
+    hashRounds: 10,
+    port: 5000
 };
 
 module.exports = config;
