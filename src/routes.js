52364c5e983f896499981fa99a50fd1ff0c4c737
rahmatsyifana12 <adean435@gmail.com>
2022-06-04 12:26:47 +0700

    add routing for edit profile and change routing add user into register

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const  { Router } = require('express');
 const { addNewMotorcycle, getAllMotorcyclesForUser, getMotorcycleById } = require('./controllers/motorcycle.controller');
 const { getAllServices, addNewService, getAllServicesForUser } = require('./controllers/service.controller');
-const { addNewUser, loginUser, logoutUser } = require('./controllers/user.controller');
+const { addNewUser, loginUser, logoutUser, editUserProfile } = require('./controllers/user.controller');
 const { authenticate } = require('./middlewares/authenticate.middleware');
 const { validate } = require('./middlewares/validate.middleware');
 const { newMotorcycleSchema } = require('./validations/motorcycle.validation');
-const { newUserSchema } = require('./validations/user.validation');
+const { newUserSchema, editProfileSchema } = require('./validations/user.validation');
 
 const router = Router();
 
@@ -14,10 +14,12 @@ router.get('/api/v1/services/user', authenticate, getAllServicesForUser);
 router.get('/api/v1/motorcycles/user', authenticate, getAllMotorcyclesForUser);
 router.get('/api/v1/motorcycles/:motorcycleId', authenticate, getMotorcycleById);
 
-router.post('/api/v1/users/add', validate(newUserSchema), addNewUser);
+router.post('/api/v1/register', validate(newUserSchema), addNewUser);
 router.post('/api/v1/login', loginUser);
 router.post('/api/v1/motorcycles/add', authenticate, validate(newMotorcycleSchema), addNewMotorcycle);
 router.post('/api/v1/services/:motorcycleId', authenticate, addNewService);
+
+router.put('/api/v1/users/edit', authenticate, validate(editProfileSchema), editUserProfile);
 
 router.delete('/api/v1/logout', authenticate, logoutUser);
 
