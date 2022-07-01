6aaa4d613e18165cf86c70b7288d485a04172d6d
rahmatsyifana12 <adean435@gmail.com>
2022-07-01 22:46:50 +0700

    blala

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool;
 const config = require('./configs/config');
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false} });
 
 module.exports = pool;
