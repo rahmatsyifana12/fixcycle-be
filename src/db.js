cb6de4829c4c3884226b0fb228899f78ad567964
rahmatsyifana12 <adean435@gmail.com>
2022-07-01 22:44:23 +0700

    Setting database url

--- src/db.js
+++ src/db.js
@@ -1,12 +1,6 @@
 const Pool = require('pg').Pool;
 const config = require('./configs/config');
 
-const pool = new Pool({
-    user: config.db.username,
-    password: config.db.password,
-    host: config.db.host,
-    port: config.db.port,
-    database: config.db.database
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 module.exports = pool;
